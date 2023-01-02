50dc9fbb86593cec166de4d58c4234f3b350645d
SojerSM <seb.maz1996@gmail.com>
2023-01-02 20:37:28 +0100

    Note expanding smooth transition added

--- src/components/NoteList/NoteItem.js
+++ src/components/NoteList/NoteItem.js
@@ -1,16 +1,17 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { API_URL } from '../../utils/config';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 import useHttp from '../../hooks/use-http';
 
 import styles from './NoteItem.module.css';
-import { removeIcon } from '../../assets/icons';
+import * as icons from '../../assets/icons';
 
 import NoteContext from '../../store/note-context';
 
 import Section from '../../layout/Section';
 
 const NoteItem = function (props) {
+  const [isExpanded, setIsExpanded] = useState(false);
   const { removeNote } = useContext(NoteContext);
   const { sendRequest: sendRemoveRequest } = useHttp();
 
@@ -30,18 +31,35 @@ const NoteItem = function (props) {
     );
   };
 
+  const toggleNoteExpandHandler = (event) => {
+    event.preventDefault();
+    setIsExpanded((prevState) => !prevState);
+  };
+
+  const contentHeight = isExpanded && '10rem';
+
   return (
     <Section className={styles['note']}>
       <div>
         <h2>{capitalizeFirstLetter(props.title)}</h2>
-        <button className={styles['remove-btn']} onClick={removeNoteHandler}>
-          <svg className={styles['svg-icon']} viewBox="0 0 20 20">
-            {removeIcon}
-          </svg>
-        </button>
+        <div>
+          <button
+            className={styles['spread-btn']}
+            onClick={toggleNoteExpandHandler}
+          >
+            <svg className={styles['svg-icon']} viewBox="0 0 20 20">
+              {isExpanded ? icons.spreadIconExpanded : icons.spreadIconHidden}
+            </svg>
+          </button>
+          <button className={styles['remove-btn']} onClick={removeNoteHandler}>
+            <svg className={styles['svg-icon']} viewBox="0 0 20 20">
+              {icons.removeIcon}
+            </svg>
+          </button>
+        </div>
       </div>
       <div>
-        <p className={styles['content']}>
+        <p className={styles['content']} style={{ maxHeight: contentHeight }}>
           {capitalizeFirstLetter(props.content)}
         </p>
       </div>
